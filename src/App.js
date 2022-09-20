ef8012848c9226188a5881fa8b05e3aeef55149f
Abril <basteiroabril@gmail.com>
2022-09-20 12:45:40 -0300

    navbar toggler

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Works from "./pages/Works";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/works' element={<Works/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </>
   );
 }
 
