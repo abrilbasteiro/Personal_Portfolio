3f47cefdda16e401c158ea7f1a0102903cc2b8fe
Abril <basteiroabril@gmail.com>
2022-12-11 16:32:35 -0300

    works section

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -27,9 +27,9 @@ const Navbar = () => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarMenu">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        {/* <li className="nav-item">
-                            <Link className="nav-link" to='/'>Home</Link>
-                        </li> */}
+                        <li className="nav-item">
+                            <Link className="nav-link" to='hero'>Home</Link>
+                        </li>
                         <li className="nav-item">
                             <Link className="nav-link" to='about'>About me</Link>
                         </li>
