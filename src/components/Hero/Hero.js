c2fb82900478f44b841b9fd4d94405f0c3c446ea
Abril <basteiroabril@gmail.com>
2022-12-12 11:06:06 -0300

    welcome animation

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,7 +5,12 @@ const Hero = () => {
   return (
     <section className='hero' id='hero'>
         <div className='hero'>
-          <p className='heroTitle'>&#45; Welcome! &#45;</p>
+          <div className='welcome'>
+            <p className='heroTitle'>&#45; Welcome! &#45;</p>
+            <p className='closeBtn'>X</p>
+          </div>
+            {/* <p className='heroTitle'>&#45; Welcome! &#45;</p>
+            <p className='closeBtn'>X</p> */}
           <h1 className='heroName'>I am Abril</h1>
           <h2 className='heroSubtitle'>frontend developer</h2>
         </div>
