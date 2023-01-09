30781784fcee4d38b1ffe3984b1d9efaf18ca4d2
Abril <basteiroabril@gmail.com>
2023-01-09 20:01:21 -0300

    contact section and hero animations

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,46 @@
 import React from 'react'
 import './Contact.scss'
+import { Button, Form } from 'react-bootstrap'
+
 const Contact = () => {
   return (
     <section className='contact' id='contact'>
-      Contact
+        <h2>Let's work together!</h2>
+        <h3>Leave me a message</h3>
+
+        <form className="row contactForm">
+          <div className="formLeft col-6">
+            <Form.Group className="mb-3" controlId='name'>
+              <Form.Control type="text" placeholder="Name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId='email'>
+              <Form.Control type="email" placeholder="Email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId='phone'>
+              <Form.Control type="tel" placeholder="Contact phone" />
+            </Form.Group>
+            <Form.Group controlId="where">
+              <Form.Label>Where did you find me?</Form.Label>
+              <Form.Control as="select" className='select'>
+                <option key='blankChoice' hidden value>Please, select one</option>
+                <option>LinkedIn</option>
+                <option>ZonaJobs</option>
+                <option>Bumeran</option>
+                <option>Computrabajo</option>
+                <option>Other</option>
+              </Form.Control>
+            </Form.Group>
+          </div>
+          <div className="formRight col-6">
+            <Form.Group className="mb-3 formText" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>How can I help you?</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <Button variant="primary" type="submit" className='submitBtn'>
+              Submit
+            </Button>
+          </div>
+        </form>
     </section>
   )
 }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './Hero.scss';
+import { GiArrowCursor } from 'react-icons/gi'
 
 const Hero = () => {
   return (
-    <section className='hero' id='hero'>
-        <div className='hero'>
+    <section className='heroSection' id='hero'>
           <div className='welcome'>
             <p className='heroTitle'>&#45; Welcome! &#45;</p>
             <p className='closeBtn'>X</p>
+            <GiArrowCursor className="mouseCursor"/>
           </div>
-            {/* <p className='heroTitle'>&#45; Welcome! &#45;</p>
-            <p className='closeBtn'>X</p> */}
           <h1 className='heroName'>I am Abril</h1>
           <h2 className='heroSubtitle'>frontend developer</h2>
-        </div>
     </section>
   )
 }
